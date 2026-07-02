ecf9b70512e3a8a25986e17e5c5b9c8707bda7b5
MrX-zeta <brianluisruizperez@gmail.com>
2026-07-02 00:53:16 -0600

    feat(db): expand exercise catalog with full gym equipment variants

--- app/src/main/java/com/luis/itera/data/local/ExerciseSeed.kt
+++ app/src/main/java/com/luis/itera/data/local/ExerciseSeed.kt
@@ -4,23 +4,59 @@ import com.luis.itera.data.local.entity.ExerciseEntity
 
 object ExerciseSeed {
     val exercises = listOf(
-        ExerciseEntity(
-            name = "Dominadas pronas",
-            category = "Calistenia",
-            equipment = "Barra",
-            mainMuscleGroup = "Espalda"
-        ),
-        ExerciseEntity(name = "Dominadas supinas", category = "Calistenia", equipment = "Barra", mainMuscleGroup = "Espalda"),
+        ExerciseEntity(name = "Dominadas agarre prono", category = "Calistenia", equipment = "Barra", mainMuscleGroup = "Espalda"),
+        ExerciseEntity(name = "Dominadas agarre supino", category = "Calistenia", equipment = "Barra", mainMuscleGroup = "Espalda"),
         ExerciseEntity(name = "Fondos en paralelas", category = "Calistenia", equipment = "Peso Corporal", mainMuscleGroup = "Pecho"),
         ExerciseEntity(name = "Flexiones", category = "Calistenia", equipment = "Peso Corporal", mainMuscleGroup = "Pecho"),
+        ExerciseEntity(name = "Flexiones declinadas", category = "Calistenia", equipment = "Peso Corporal", mainMuscleGroup = "Pecho"),
         ExerciseEntity(name = "Muscle up", category = "Calistenia", equipment = "Barra", mainMuscleGroup = "Espalda"),
         ExerciseEntity(name = "Sentadilla pistol", category = "Calistenia", equipment = "Peso Corporal", mainMuscleGroup = "Piernas"),
+
         ExerciseEntity(name = "Press banca", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Pecho"),
-        ExerciseEntity(name = "Sentadilla con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Piernas"),
+        ExerciseEntity(name = "Press banca inclinado con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Pecho"),
+        ExerciseEntity(name = "Press banca inclinado con mancuernas", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Pecho"),
+        ExerciseEntity(name = "Press banca inclinado en Smith", category = "Gimnasio", equipment = "Máquina", mainMuscleGroup = "Pecho"),
+        ExerciseEntity(name = "Peck deck", category = "Gimnasio", equipment = "Máquina", mainMuscleGroup = "Pecho"),
+        ExerciseEntity(name = "Aperturas con mancuernas", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Pecho"),
+
         ExerciseEntity(name = "Peso muerto", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Espalda"),
-        ExerciseEntity(name = "Press militar", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Hombros"),
-        ExerciseEntity(name = "Curl con mancuerna", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Bíceps"),
+        ExerciseEntity(name = "Peso muerto rumano con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Femoral"),
+        ExerciseEntity(name = "Peso muerto rumano con mancuernas", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Femoral"),
+        ExerciseEntity(name = "Peso muerto sumo con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Piernas"),
+        ExerciseEntity(name = "Peso muerto sumo con mancuernas", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Piernas"),
+
+        ExerciseEntity(name = "Remo en T", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Espalda"),
+        ExerciseEntity(name = "Remo con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Espalda"),
+        ExerciseEntity(name = "Remo en máquina", category = "Gimnasio", equipment = "Máquina", mainMuscleGroup = "Espalda"),
         ExerciseEntity(name = "Remo con mancuerna", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Espalda"),
+        ExerciseEntity(name = "Pull over con soga en polea", category = "Gimnasio", equipment = "Polea", mainMuscleGroup = "Espalda"),
+        ExerciseEntity(name = "Jalón al pecho agarre prono", category = "Gimnasio", equipment = "Polea", mainMuscleGroup = "Espalda"),
+        ExerciseEntity(name = "Jalón al pecho agarre supino", category = "Gimnasio", equipment = "Polea", mainMuscleGroup = "Espalda"),
+        ExerciseEntity(name = "Jalón al pecho agarre neutro", category = "Gimnasio", equipment = "Polea", mainMuscleGroup = "Espalda"),
+
+        ExerciseEntity(name = "Press militar con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Hombros"),
+        ExerciseEntity(name = "Press militar con mancuernas", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Hombros"),
+        ExerciseEntity(name = "Elevaciones laterales con mancuerna", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Hombros"),
+        ExerciseEntity(name = "Elevaciones laterales en polea", category = "Gimnasio", equipment = "Polea", mainMuscleGroup = "Hombros"),
+        ExerciseEntity(name = "Pájaros con mancuernas", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Hombros"),
+        ExerciseEntity(name = "Deltoides posterior en máquina", category = "Gimnasio", equipment = "Máquina", mainMuscleGroup = "Hombros"),
+        ExerciseEntity(name = "Encogimientos con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Trapecios"),
+        ExerciseEntity(name = "Encogimientos con mancuernas", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Trapecios"),
+
+        ExerciseEntity(name = "Curl de bíceps con mancuerna", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Bíceps"),
+        ExerciseEntity(name = "Curl de bíceps con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Bíceps"),
+        ExerciseEntity(name = "Curl martillo", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Bíceps"),
+        ExerciseEntity(name = "Curl Zottman", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Bíceps"),
+        ExerciseEntity(name = "Curl de muñecas con mancuernas", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Antebrazo"),
+        ExerciseEntity(name = "Curl de muñecas con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Antebrazo"),
+
+        ExerciseEntity(name = "Sentadilla con barra", category = "Gimnasio", equipment = "Barra", mainMuscleGroup = "Piernas"),
+        ExerciseEntity(name = "Sentadilla Hack", category = "Gimnasio", equipment = "Máquina", mainMuscleGroup = "Piernas"),
+        ExerciseEntity(name = "Sentadillas búlgaras con mancuernas", category = "Gimnasio", equipment = "Mancuerna", mainMuscleGroup = "Piernas"),
+        ExerciseEntity(name = "Prensa de piernas", category = "Gimnasio", equipment = "Máquina", mainMuscleGroup = "Piernas"),
+        ExerciseEntity(name = "Curl femoral acostado", category = "Gimnasio", equipment = "Máquina", mainMuscleGroup = "Femoral"),
+        ExerciseEntity(name = "Costurera", category = "Gimnasio", equipment = "Máquina", mainMuscleGroup = "Gastrocnemio"),
+
         ExerciseEntity(name = "Carrera continua", category = "Cardio", equipment = "Peso Corporal", mainMuscleGroup = "Piernas"),
         ExerciseEntity(name = "Cuerda (saltos)", category = "Cardio", equipment = "Peso Corporal", mainMuscleGroup = "Piernas")
     )
